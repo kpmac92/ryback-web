a2edcf9d4d7b5aea408695a95e4ca3a3091a1e40
Kevin Patrick <kpmac92@gmail.com>
2020-03-29 19:27:19 -0400

    Add basic redux store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
 import './App.scss';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './store/reducers';
 
-const App = () => <NavBar />;
+const App = () => {
+  const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
+  return (
+    <Provider store={store}>
+      <NavBar />
+    </Provider>
+  );
+};
 
 export default App;
---
+++ src/store/reducers/recipeReducer.js
@@ -0,0 +1,13 @@
+const defaultState = {
+  allRecipes: [
+    { name: 'Pancakes' },
+    { name: 'Creamy Cajun Pasta' },
+    { name: 'Honey Sesame Chicken' },
+  ],
+};
+
+const recipeReducer = (state = defaultState, action) => {
+  return state;
+};
+
+export default recipeReducer;
