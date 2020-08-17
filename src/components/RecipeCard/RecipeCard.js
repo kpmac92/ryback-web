b607432743afe7c96ac5fca941bead4108616d3f
Kevin Patrick <kpmac92@gmail.com>
2020-08-16 20:07:03 -0400

    refine layout and styling for recipe card

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -3,12 +3,15 @@ import './RecipeCard.scss';
 
 const RecipeCard = ({ recipe }) => (
   <div className="recipe-card">
-    <span className="recipe-title">{recipe.name}</span>
+    <div className="card-header">
+      <span className="recipe-title">{recipe.name}</span>
+    </div>
     <div className="horiz-flex-container">
       <div className="description-container">
         <span className="description">{recipe.description}</span>
       </div>
       <div className="ingredient-container">
+        <span>Main Ingredients: </span>
         <ul>
           {recipe.ingredients.map((ingredient, i) =>
             ingredient.main ? <li key={i}>{ingredient.name}</li> : null
