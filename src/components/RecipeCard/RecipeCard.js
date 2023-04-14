1beb9dd895a96d55fcfe672598d6710b4a1a0f50
Kevin Patrick <kpmac92@gmail.com>
2023-04-14 13:35:23 -0400

    Add delete to recipe card

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import './RecipeCard.scss';
 
+const deleteRecipe = (id) => {
+  fetch('http://localhost:8080/recipes/delete', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(id),
+  });
+  //TODO: refetch recipe list after delete
+};
+
 const RecipeCard = ({ recipe }) => (
   <div className="recipe-card">
     <div className="card-header">
@@ -18,6 +29,7 @@ const RecipeCard = ({ recipe }) => (
             ingredient.main ? <li key={i}>{ingredient.name}</li> : null
           )}
         </ul>
+        <button onClick={() => deleteRecipe(recipe.id)}>Delete</button>
       </div>
     </div>
   </div>
