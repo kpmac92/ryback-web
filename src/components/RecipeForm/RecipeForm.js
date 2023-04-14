d0f47948c93a229acbd46fe5ce309622ee99daf7
Kevin Patrick <kpmac92@gmail.com>
2023-04-14 11:13:41 -0400

    connect recipe form to backend

--- src/components/RecipeForm/RecipeForm.js
+++ src/components/RecipeForm/RecipeForm.js
@@ -2,26 +2,34 @@ import React, { useState } from 'react';
 import './RecipeForm.scss';
 
 const RecipeForm = () => {
-  const onSubmit = () => {
-    body = { name: formInput.name, description: formInput.description };
-    //todo: call backend
-  };
-
   const [formInput, setFormInput] = useState({});
+
+  const onSubmit = () => {
+    const body = { name: formInput.name, description: formInput.description };
+
+    fetch('http://localhost:8080/recipes/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+  };
 
   const onInputChange = (e) => {
     setFormInput({ ...formInput, [e.target.name]: e.target.value });
   };
+
   return (
     <div className="recipe-form">
       <div className="input-section" onChange={onInputChange}>
-        Name: <input name="nameInput" />
+        Name: <input name="name" />
       </div>
       <div className="input-section">
-        description: <input name="descriptionInput" onChange={onInputChange} />
+        description: <input name="description" onChange={onInputChange} />
       </div>
 
-      <button onClick={() => console.log(formInput)}>submit</button>
+      <button onClick={onSubmit}>submit</button>
     </div>
   );
 };
